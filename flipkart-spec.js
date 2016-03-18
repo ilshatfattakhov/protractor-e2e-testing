903441e7d12d1c9f67c43af1379b23d2817e7539
Ilshat Fattakhov <i.fattahov@gmail.com>
2016-03-18 18:09:12 +0500

    Adds some tests

--- flipkart-spec.js
+++ flipkart-spec.js
@@ -1,5 +1,5 @@
 var LoginPage = require('./pages/login.page');
- 
+var MobilesPage = require('.pages/mobiles.page'); 
 describe('login page', function() {
 	beforeEach(function() {
 	  return browser.ignoreSynchronization = true;
@@ -8,7 +8,7 @@ describe('login page', function() {
   it('should show "invalid details" error if password is incorrect', function(){
   	var page = new LoginPage();
 	page.visit();
-    page.setPhone('7696373114');
+    page.setPhone('mobile');
     page.setPassword('WrongPassword');
     page.login();
     expect(page.errorText.isDisplayed()).toBe(true);
@@ -24,14 +24,16 @@ describe('login page', function() {
     expect(page.errorText.getText()).toEqual('Please enter a valid email/mobile');
   })
 
-
   it('should show greeting text if phone and password are correct', function() {
 	var page = new LoginPage();
     page.visit();
-    page.setPhone('7696373114');
-    page.setPassword('admin1234');
+    page.setPhone('mobile');
+    page.setPassword('password');
     page.login();
     expect(page.getGreeting()).toEqual('Hi Nic!');
   });
+});
 
+describe('Mobiles page', function(){
+	mobilesPage = new MobilesPage()
 });
